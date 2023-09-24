d911642e059019e8a2cc232a92aa7ff39d001231
Jerry Ma <leilingjerry@gmail.com>
2023-09-24 11:20:09 +0930

    add a project with master class

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+interface HeaderProps {
+  category: string;
+  query: string;
+}
+
+
+const Header = ({ category, query }: HeaderProps) => {
+  if(query && category) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        Search results for "{query}" in <span className="capitalize">{category}</span>
+      </h1>
+    )
+  }
+
+  if(query) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        Search results for "{query}"
+      </h1>
+    )
+  }
+
+  if(category) {
+    return (
+      <h1 className="heading3 self-start text-white-800">
+        <span className="capitalize">{category}</span>
+      </h1>
+    )
+  }
+  
+  return (
+    <h1 className="heading3 self-start text-white-800">No Results</h1>
+  )
+}
+
+export default Header
